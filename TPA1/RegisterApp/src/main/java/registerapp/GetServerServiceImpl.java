dfaa3b762d54a67d0e32aa675dcf016ab24b3fb2
sofiafernandescd <sofiafernandescd@gmail.com>
2023-11-11 11:45:13 +0000

    versao que compila

--- TPA1/RegisterApp/src/main/java/registerapp/GetServerServiceImpl.java
+++ TPA1/RegisterApp/src/main/java/registerapp/GetServerServiceImpl.java
@@ -1,32 +1,32 @@
 package registerapp;
 
-import clientToManager.GetServerServiceGrpc;
-import clientToManager.ServerInfo;
-import clientToManager.Void;
+import ContractClientRegister.GetServerServiceGrpc;
+import ContractClientRegister.ServerInfo;
+import ContractClientRegister.Empty;
 import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.stub.StreamObserver;
 
 public class GetServerServiceImpl extends GetServerServiceGrpc.GetServerServiceImplBase {
 
-    private final RingInfo ringInfo;
+    private final RegisterInfo registerInfo;
     private int counter;
 
-    public GetServerServiceImpl(RingInfo ringInfo) {
-        this.ringInfo = ringInfo;
+    public GetServerServiceImpl(RegisterInfo registerInfo) {
+        this.registerInfo = registerInfo;
     }
 
-    public synchronized void getKvServer(Void request, StreamObserver<ServerInfo> resSO) {
+    public synchronized void getServerEndpoint(Empty request, StreamObserver<ServerInfo> result) {
         // se o anel ainda não estiver formado
-        if (ringInfo.getNumberOfServers() < ringInfo.getNkvServers()) {
+        if (registerInfo.getNumberOfServers() < registerInfo.getNkvServers()) {
             Throwable th = new StatusException(Status.UNAVAILABLE);
-            resSO.onError(th);
+            result.onError(th);
         }
         else {
             //Dar a volta à lista
-            if (counter == ringInfo.getNkvServers()) counter = 0;
+            if (counter == registerInfo.getNkvServers()) counter = 0;
 
-            String[] server = ringInfo.getListOfServers().get(counter).split(":");
+            String[] server = registerInfo.getListOfServers().get(counter).split(":");
 
             ServerInfo reply = ServerInfo.newBuilder()
                     .setServerIP(server[0])
@@ -34,8 +34,58 @@ public class GetServerServiceImpl extends GetServerServiceGrpc.GetServerServiceI
                     .build();
 
             counter++;
-            resSO.onNext(reply);
-            resSO.onCompleted();
+            result.onNext(reply);
+            result.onCompleted();
+        }
+    }
+
+    // TODO
+    public synchronized void failInform(ServerInfo serverInfo, StreamObserver<ServerInfo> result) {
+        // o servidor que chama este serviço informa no pedido o servidor sucessor que falhou
+        String offlineServer = serverInfo.getServerIP() + ':' + serverInfo.getServerPort();
+
+        LinkedList<String> ServersList = registerInfo.getListOfServers();
+
+        // se apenas existir um servidor online e outro offline na lista
+        // retornar erro visto que não haverá um próximo para retornar (apenas aplicável a esta implementação)
+        if (ServersList.size() == 2) {
+            Throwable th = new StatusException(
+                    Status.PERMISSION_DENIED.withDescription("There are no more servers on the ring")
+            );
+
+            result.onError(th);
+        } else {
+            // se o servidor que falhar for o último da lista
+            if (ServersList.getLast().equals(offlineServer)) {
+                String[] nextServer = ServersList.getFirst().split(":");
+
+                ServerInfo response = ServerInfo.newBuilder()
+                        .setServerInfoIP(nextServer[0])
+                        .setServerInfoPort(Integer.parseInt(nextServer[1]))
+                        .build();
+
+                result.onNext(response);
+                result.onCompleted();
+
+                ServersList.removeLast();
+                registerInfo.decrementNkvServer();
+                System.out.println("> updated KvServer list: " + ServersList);
+            } else {
+                int index = ServersList.indexOf(offlineServer);
+                String[] nextServer = ServersList.get(++index).split(":");
+
+                ServerInfo response = ServerInfo.newBuilder()
+                        .setServerInfoIP(nextServer[0])
+                        .setServerInfoPort(Integer.parseInt(nextServer[1]))
+                        .build();
+
+                result.onNext(response);
+                result.onCompleted();
+
+                ServersList.remove(--index);
+                registerInfo.decrementNServer();
+                System.out.println("> updated KvServer list: " + ServersList);
+            }
         }
     }
 
--- TPA1/RegisterApp/src/main/java/registerapp/RegistServerServiceImpl.java
+++ TPA1/RegisterApp/src/main/java/registerapp/RegistServerServiceImpl.java
@@ -6,13 +6,13 @@ import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import ContractRegisterServer.ServerInfo;
 import ContractRegisterServer.ServerResponse;
-//import ContractRegisterServer.NextServer;
-import ContractRegisterServer.RegisterServiceGrpc;
+//import ContractRegisterServer.ServerInfo;
+import ContractRegisterServer.RegistServerServiceGrpc;
 
 // LinkedList to keep the Servers
 import java.util.LinkedList;
 
-public class RegistServerServiceImpl extends RegisterServiceGrpc.RegisterServiceImplBase {
+public class RegistServerServiceImpl extends RegistServerServiceGrpc.RegistServerServiceImplBase {
 
     private final RegisterInfo registerInfo;
 
@@ -34,98 +34,50 @@ public class RegistServerServiceImpl extends RegisterServiceGrpc.RegisterService
 
     }
 
-    public synchronized void getNextServer(KvServerInfo serverInfo, StreamObserver<NextServer> resSO) {
-        String requestingServer = serverInfo.getServerIP() + ':' + serverInfo.getServerPort();
+    public synchronized void getNextServer(ServerInfo serverInfo, StreamObserver<ServerInfo> result) {
+        String requestServer = serverInfo.getServerIP() + ':' + serverInfo.getServerPort();
         LinkedList<String> ServersList = registerInfo.getListOfServers();
 
         // se o servidor que faz o pedido não estiver registado, retornar erro
-        if (!ServersList.contains(requestingServer)) {
+        if (!ServersList.contains(requestServer)) {
             Throwable th = new StatusException(Status.PERMISSION_DENIED
                     .withDescription("Can't get next server because you are not registered"));
 
-            resSO.onError(th);
+            result.onError(th);
         }
         // se faltarem servidores por se registar, informar para fazer um novo pedido
         else if (registerInfo.getNumberOfServers() < registerInfo.getNServers()) {
             Throwable th = new StatusRuntimeException(Status.UNAVAILABLE
                     .withDescription("The ring hasn't formed yet, try again later"));
 
-            resSO.onError(th);
+            result.onError(th);
         } else {
             // se o servidor que faz o pedido  do próximo servidor for o último da lista, retornar o primeiro da lista
-            if (ServersList.getLast().equals(requestingServer)) {
+            if (ServersList.getLast().equals(requestServer)) {
                 String[] nextServer = ServersList.getFirst().split(":");
 
-                NextServer response = NextServer.newBuilder()
-                        .setNextServerIP(nextServer[0])
-                        .setNextServerPort(Integer.parseInt(nextServer[1]))
+                ServerInfo response = ServerInfo.newBuilder()
+                        .setServerInfoIP(nextServer[0])
+                        .setServerInfoPort(Integer.parseInt(nextServer[1]))
                         .build();
 
-                resSO.onNext(response);
-                resSO.onCompleted();
+                result.onNext(response);
+                result.onCompleted();
             }else {
                 // apenas retornar o próximo elemento da lista
-                int index = ServersList.indexOf(requestingServer);
+                int index = ServersList.indexOf(requestServer);
                 String[] nextServer = ServersList.get(++index).split(":");
 
-                NextServer response = NextServer.newBuilder()
-                        .setNextServerIP(nextServer[0])
-                        .setNextServerPort(Integer.parseInt(nextServer[1]))
+                ServerInfo response = ServerInfo.newBuilder()
+                        .setServerInfoIP(nextServer[0])
+                        .setServerInfoPort(Integer.parseInt(nextServer[1]))
                         .build();
 
-                resSO.onNext(response);
-                resSO.onCompleted();
+                result.onNext(response);
+                result.onCompleted();
             }
         }
     }
 
-    public synchronized void failInform(KvServerInfo serverInfo, StreamObserver<NextServer> resSO) {
-        // o servidor que chama este serviço informa no pedido o servidor sucessor que falhou
-        String offlineServer = serverInfo.getServerIP() + ':' + serverInfo.getServerPort();
-
-        LinkedList<String> ServersList = registerInfo.getListOfServers();
-
-        // se apenas existir um servidor online e outro offline na lista
-        // retornar erro visto que não haverá um próximo para retornar (apenas aplicável a esta implementação)
-        if (ServersList.size() == 2) {
-            Throwable th = new StatusException(
-                    Status.PERMISSION_DENIED.withDescription("There are no more servers on the ring")
-            );
-
-            resSO.onError(th);
-        } else {
-            // se o servidor que falhar for o último da lista
-            if (ServersList.getLast().equals(offlineServer)) {
-                String[] nextServer = ServersList.getFirst().split(":");
-
-                NextServer response = NextServer.newBuilder()
-                        .setNextServerIP(nextServer[0])
-                        .setNextServerPort(Integer.parseInt(nextServer[1]))
-                        .build();
-
-                resSO.onNext(response);
-                resSO.onCompleted();
-
-                ServersList.removeLast();
-                registerInfo.decrementNkvServer();
-                System.out.println("> updated KvServer list: " + ServersList);
-            } else {
-                int index = ServersList.indexOf(offlineServer);
-                String[] nextServer = ServersList.get(++index).split(":");
-
-                NextServer response = NextServer.newBuilder()
-                        .setNextServerIP(nextServer[0])
-                        .setNextServerPort(Integer.parseInt(nextServer[1]))
-                        .build();
-
-                resSO.onNext(response);
-                resSO.onCompleted();
-
-                ServersList.remove(--index);
-                registerInfo.decrementNServer();
-                System.out.println("> updated KvServer list: " + ServersList);
-            }
-        }
-    }
 
 }
