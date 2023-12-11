c96db56966974057e13a581bcfca32ec36ae3ff3
Argennt <yetisadida@gmail.com>
2023-12-11 00:22:13 +0000

    alteracoes worker
    alterações no java do worker e das dependências no pom (add spread 4.4.0)

--- Lab4/SpreadDemo/maven-install-lib.bat
+++ Lab4/SpreadDemo/maven-install-lib.bat
@@ -1,3 +1,3 @@
 set JAVA_HOME=C:\Program Files\Java\jdk-11.0.10
 set PATH="D:\maven\apache-maven-3.8.1\bin";%PATH%
-mvn install:install-file -Dfile=spread-4.4.0.jar -DgroupId=org.spread -DartifactId=spread -Dversion=4.4.0 -Dpackaging=jar
+mvn install:install-file -Dfile=spread-5.0.1.jar -DgroupId=org.spread -DartifactId=spread -Dversion=5.0.1 -Dpackaging=jar
